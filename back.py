31bc1a1c3cecb828e0fa75950fc8864478e0dd6a
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-10 19:42:48 +0500

    back.py

---
+++ back.py
@@ -0,0 +1,22 @@
+#--------backend--------------
+import requests
+import psycopg2
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+	conn = psycopg2.connect(dbname='tg_bot',
+                        user='postgres', 
+                        password='password',
+                        host='64.227.127.179',
+                        port='5432')
+	cursor = conn.cursor()
+
+	cursor.execute("SELECT * FROM orders")
+	orders = cursor.fetchall()
+	cursor.close()
+    conn.close()
+    return render_template('index.html', orders=orders)
+ if __name__ == '__main__':
+    app.run(port=80)
