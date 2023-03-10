00d25f9dcbc313b6f56964cb31063ce89738180f
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-11 02:41:54 +0500

    back.py

--- back.py
+++ back.py
@@ -2,28 +2,20 @@
 import requests
 import psycopg2
 from flask import Flask, render_template, jsonify
-
-app = Flask(__name__)
-@app.route('/orders')
-def get_orders():
-	conn = psycopg2.connect(
+conn = psycopg2.connect(
       dbname='tg_bot',
       user='postgres', 
       password='password',
       host='64.227.127.179',
       port='5432'
    )
-
-   cursor = conn.cursor()
-   
+cursor = conn.cursor()
+app = Flask(__name__)
+@app.route('/orders')
+def get_orders():
    cursor.execute("SELECT * FROM orders")
-   
    rows = cursor.fetchall()
-   
-   cursor.close()
-   
    data = []
-   
    for row in rows:
       data.append({'id': row[0], 'description': row[1], 'weight': row[2], 'from_address': row[3], 'to_address': row[4], 'phone': row[5]})
    return jsonify({'data': data})
