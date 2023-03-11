e24fe3b248c0110d56033f47cc8186a7828aac40
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-12 00:56:56 +0500

    changes

--- app/back.py
+++ app/back.py
@@ -3,18 +3,23 @@ import requests
 import psycopg2
 from flask import Flask, render_template, jsonify
 
-conn = psycopg2.connect(
-      dbname='tg_bot',
-      user='postgres', 
-      password='password',
-      host='64.227.127.179',
-      port='5432'
-   )
-cursor = conn.cursor()
+# conn = psycopg2.connect(
+#       dbname='tg_bot',
+#       user='postgres', 
+#       password='password',
+#       host='64.227.127.179',
+#       port='5432'
+#    )
+# cursor = conn.cursor()
 
 app = Flask(__name__)
 @app.route('/')
-   return "Hello"
+def index():
+   return "Hello 234234"
+
+@app.route('/about')
+def about():
+   return "about page"
 
 
 if __name__ == '__main__':
