035c63fe7ad595b752fcc43d37cd6358354883d2
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-11 19:15:09 +0500

    Changes

--- app/back.py
+++ app/back.py
@@ -2,6 +2,7 @@
 import requests
 import psycopg2
 from flask import Flask, render_template, jsonify
+
 conn = psycopg2.connect(
       dbname='tg_bot',
       user='postgres', 
@@ -10,7 +11,9 @@ conn = psycopg2.connect(
       port='5432'
    )
 cursor = conn.cursor()
+
 app = Flask(__name__)
+
 @app.route('/orders')
 def get_orders():
    cursor.execute("SELECT * FROM orders")
@@ -20,5 +23,7 @@ def get_orders():
       data.append({'id': row[0], 'description': row[1], 'weight': row[2], 'from_address': row[3], 'to_address': row[4], 'phone': row[5]})
    return jsonify({'data': data})
    return render_template("index.html", data=data)
+
+
 if __name__ == '__main__':
     app.run()
