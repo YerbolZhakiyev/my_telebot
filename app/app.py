e14ad27be667f50738d92c93e47c339d4535bca8
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-12 22:36:08 +0500

    changes

--- app/app.py
+++ app/app.py
@@ -1,22 +1,28 @@
 import psycopg2
 import json
 import time
+from flask import Flask, render_template, jsonify
 
 conn = psycopg2.connect(dbname='tg_bot',
-                        user='postgres', 
+                        user='erbol', 
                         password='password',
                         host='64.227.127.179',
                         port='5432')
 
 cursor = conn.cursor()
 
-while True:
-    cursor.execute("SELECT * FROM orders")
+app = Flask(__name__)
+@app.route('/')
+def index():
+
+	cursor.execute("SELECT * FROM orders")
     rows = cursor.fetchall()
     results = []
     for row in rows:
         results.append({'id': row[0], 'description': row[1], 'from_address': row[2], 'to_address': row[3], 'weight': row[4], 'phone': row[5]})
-    time.sleep(60)
 
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(results, f, ensure_ascii=False)
+    return results
+    # render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- web/web.py
+++ web/web.py
@@ -1,17 +1,5 @@
 import requests
-import psycopg2
-from flask import Flask, render_template, jsonify
 import json
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    data = json.load(open('data.json', 'r'))
-    return render_template('index.html', data=data)
-
-@app.route('/about')
-def about():
-   return "about page"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+r = request.get('http://my_web')
+print(r.json())
