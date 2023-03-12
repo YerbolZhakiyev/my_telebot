bfecc6362d8e4a5c29d3fba36b7d1aa091f90d2b
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-12 16:53:02 +0500

    Changes

--- app/app.py
+++ app/app.py
@@ -6,12 +6,12 @@ conn = psycopg2.connect(dbname='tg_bot',
                         host='64.227.127.179',
                         port='5432')
 cursor = conn.cursor()
-cursor.execute("SELECT * FROM orders")
-rows = cursor.fetchall()
-results = []
-for row in rows:
-    results.append({'id': row[0], 'description': row[1], 'from_address': row[2], 'to_address': row[3], 'weight': row[4], 'phone': row[5]})
+while True:
+	cursor.execute("SELECT * FROM orders")
+	rows = cursor.fetchall()
+	results = []
+	for row in rows:
+    	results.append({'id': row[0], 'description': row[1], 'from_address': row[2], 'to_address': row[3], 'weight': row[4], 'phone': row[5]})
+    time.sleep(60)
 with open('data.json', 'w', encoding='utf-8') as f:
     json.dump(results, f, ensure_ascii=False)
-cursor.close()
-conn.close()
