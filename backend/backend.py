3f1268d71cd590c3e31c9dc4933799b61f508dce
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-14 17:41:10 +0500

    Some changes

--- backend/backend.py
+++ backend/backend.py
@@ -1,5 +1,6 @@
 import psycopg2
 from flask import Flask, render_template, jsonify
+import json
 
 conn = psycopg2.connect(dbname='tg_bot',
                         user='erbol', 
@@ -17,8 +18,8 @@ def index():
 	results = []
 	for row in rows:
 		results.append({'id': row[0], 'description': row[1], 'from_address': row[2], 'to_address': row[3], 'weight': row[4], 'phone': row[5]})
-
-	return results
+	json_data = json.dumps(results, ensure_ascii=False)
+	return json_data
 
 if __name__ == '__main__':
 	app.run(debug=True)
