469799065fa7dd45af431bc545b2778d5e96ef1c
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-15 02:24:14 +0500

    Changes

--- backend/backend.py
+++ backend/backend.py
@@ -24,9 +24,9 @@ def index():
 	results = []
 	for row in rows:
 		results.append({'id': row[0], 'description': row[1], 'from_address': row[2], 'to_address': row[3], 'weight': row[4], 'phone': row[5]})
-	json_data = json.dumps(results, ensure_ascii=False)
+	# json_data = json.dumps(results, ensure_ascii=False)
 	return {
-		'data': json_data
+		'data': results
 	}
 
 if __name__ == '__main__':
