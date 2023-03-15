e23bf1eb10d45a38548ed299168fdd5f906866db
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-16 01:41:35 +0500

    Added test for flask(backend)

---
+++ test/test_flask.py
@@ -0,0 +1,11 @@
+import requests
+from dotenv import load_dotenv, find_dotenv
+import os
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+BACKEND_HOST = os.getenv('backend_host')
+response = requests.get('http://' + BACKEND_HOST + ':8000/orders')
+if response.status_code == 200:
+    print('Flask is working without any issues')
+else:
+    print('There are some troubles with backend')
