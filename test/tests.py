61bf644d67855358b931b5e99b51c7131d768f63
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-28 04:55:39 +0500

    chngs

--- test/tests.py
+++ test/tests.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def test_get_1():
-    response = requests.get('http://test_backend/orders')
+    response = requests.get('http://test_backend:8000/orders')
     json_obj = response.json()
     orders_array = json_obj['data']
     assert response.status_code == 200, 'Response status Test 1 ERROR'
@@ -10,7 +10,7 @@ def test_get_1():
     assert orders_array[1][1] == 'New York', 'Test 1 ERROR'
 
 def test_get_2():
-    response = requests.get('http://test_backend/orders')
+    response = requests.get('http://test_backend:8000/orders')
     json_obj = response.json()
     orders_array = json_obj['data']
     assert response.status_code == 200, 'Response status Test 2 ERROR'
@@ -22,7 +22,7 @@ def test_post():
     id = 123
     json_data = json.dumps({'name': name, 'tg_id': id})
     headers = {'Content-Type': 'application/json', 'Accept': 'text/plain'}
-    response = requests.post('http://test_backend/customers', data=json_data, headers=headers)
+    response = requests.post('http://test_backend:8000/customers', data=json_data, headers=headers)
     assert response.status_code == 200, 'Response status Test 3 ERROR'
 
 test_get_1()
