50a002957865288744edaa5876cd8a9be2728a76
Yerbol Zhakiyev <yerbolzhakiev@gmail.com>
2023-03-29 05:45:02 +0500

    chngs

--- test/tests.py
+++ test/tests.py
@@ -1,11 +1,14 @@
 import requests
 import json
+import logging
+
 
 def test_get_1():
+    logging.info('Starting test_get_1...')    
     response = requests.get('http://backend:8000/orders')
     json_obj = response.json()
     orders_array = json_obj['data']
-    print(orders_array)
+    logging.info('Orders array: %s', orders_array)
     assert response.status_code == 200, 'Response status Test 1 ERROR'
     assert orders_array[0]['description'] == 'Test description', 'Test 1 ERROR'
     assert orders_array[1]['from_address'] == 'New York', 'Test 1 ERROR'
@@ -14,11 +17,13 @@ def test_get_2():
     response = requests.get('http://backend:8000/orders')
     json_obj = response.json()
     orders_array = json_obj['data']
+    logging.info('Starting test_get_2...')
     assert response.status_code == 200, 'Response status Test 2 ERROR'
     assert orders_array[0]['description'] == 'ABCD', 'Test 2 success'
     assert orders_array[1]['from_address'] == 'ABCD', 'Test 2 success'
 
 def test_post():
+    logging.info('Starting test_post...')
     name = 'John Smith'
     id = 123
     json_data = json.dumps({'name': name, 'tg_id': id})
@@ -26,6 +31,8 @@ def test_post():
     response = requests.post('http://backend:8000/customers', data=json_data, headers=headers)
     assert response.status_code == 200, 'Response status Test 3 success'
 
-test_get_1()
-test_post()
-test_get_2()
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    test_get_1()
+    test_post()
+    test_get_2()
